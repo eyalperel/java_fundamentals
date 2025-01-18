9b53b45b416bc25e91e4aa1b486e7f697d4aab8a
Eyal Perelmuter <eyalperel23@gmail.com>
2025-01-18 16:38:14 -0500

    Labs: Data Types & Operators part 2

--- src/labs_examples/datatypes_operators/labs/Exercise_06.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_06.java
@@ -13,6 +13,9 @@ public class Exercise_06 {
     public static void main(String[] args) {
 
         // write code here
-
+        double volume = 3.14 * 3.14 * 3.14 * 5;
+        double area = 2 * 3.14 * 3.14 * 5 + 2 * 3.14 * 3.14 * 3.14;
+        System.out.println(volume);
+        System.out.println(area);
     }
 }
