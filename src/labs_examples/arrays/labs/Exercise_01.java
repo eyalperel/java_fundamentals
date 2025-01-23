3a3466312410eafb33f0ce9e669b8af68dba165d
Eyal Perelmuter <eyalperel23@gmail.com>
2025-01-23 11:50:45 -0500

    Labs: arrays Exercises 01-02

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,5 +1,5 @@
 package labs_examples.arrays.labs;
-
+import java.util.Scanner;
 import java.util.Arrays;
 
 /**
@@ -13,5 +13,32 @@ import java.util.Arrays;
  */
 
 public class Exercise_01 {
+    public static void main(String[] args){
+        // Create a Scanner object for input
+        Scanner scanner = new Scanner(System.in);
+        // Declare an array to store 10 numbers
+        int[] nums = new int[10];
+        int sum = 0;
+        // Prompt the user to enter 10 numbers
+        System.out.println("Enter 10 numbers:");
+        // Use a for loop to read the numbers and calculate the sum
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print("Number " + (i + 1) + ": ");
+            nums[i] = scanner.nextInt();  // Store the number in the array
+        }
+        // Use another for loop to calculate the sum of the numbers
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];  // Add each number to the sum
+        }
+        // Calculate the average
+        double average = sum / 10.0;
+
+        // Display the sum and average
+        System.out.println("Sum of the numbers: " + sum);
+        System.out.println("Average of the numbers: " + average);
+
+        // Close the scanner
+        scanner.close();
+    }
 
 }
--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,5 +1,6 @@
 package labs_examples.arrays.labs;
-
+import java.util.Scanner;
+import java.util.Arrays;
 /**
  *  More labs_examples.arrays
  *
@@ -14,8 +15,14 @@ public class Exercise_02 {
 
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-        // write code here
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number from 1-10:");
+        int target = scanner.nextInt();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                System.out.println("The index of the number you typed (" + target + ") in the array is: " + i);
+                break;
+            }
+        }
     }
 }
